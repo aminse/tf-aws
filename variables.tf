add143a4dec0694be4b2c9a75c2b9be20434b71f
Amin <amin@example.com>
2025-11-23 05:19:10 +1100

    s3 bucket name

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "tf-bucket"
+  default     = "tf-bucket-amin-v1.0"
 }
