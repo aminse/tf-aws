20f7ac6243169eace6007c7bc832dd4699f3b620
Amin <amin@example.com>
2025-11-23 04:50:46 +1100

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "tf-bucket"
+}
